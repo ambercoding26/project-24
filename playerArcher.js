59b3b092cf0bf987f2ff638d755fc8b07adc9f12
ambercoding26 <85895181+ambercoding26@users.noreply.github.com>
2021-07-06 09:56:56 -0700

    Add files via upload

---
+++ playerArcher.js
@@ -0,0 +1,41 @@
+class PlayerArcher {
+    constructor(x, y, width, height) {
+      const options = {
+        isStatic: true
+      };
+  
+      this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("./assets/playerArcher.png");
+  
+      World.add(world, this.body);
+  
+      Matter.Body.setAngle(this.body, -PI / 2);
+    }
+  
+    display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+
+     if (move === "UP" && ComputerArcher.body.angle < 1.77){
+       angleValue = 0.1;
+      } else{
+       angleValue = -0.1;
+      }
+      
+      if(move === "DOWN" && ComputerArcher.body.angle > 1.47){
+        angleValue = -0.1;
+      }else{
+        angleValue = 0.1;
+      }
+      
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
+  
